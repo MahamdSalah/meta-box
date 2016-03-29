8131580acfb8d90895eea18f25c45368508c370e
Manny Fleurmond <funkatronic@gmail.com>
2016-03-28 22:03:10 -0500

    Refresh map

--- js/map.js
+++ js/map.js
@@ -101,11 +101,26 @@
 			 */
 			$( window ).on( 'rwmb_map_refresh', function()
 			{
-				if ( that.map )
-				{
-					google.maps.event.trigger( that.map, 'resize' );
-				}
+				that.refreshMap();
 			} );
+
+			$( document ).on( 'postbox-toggled', function()
+			{
+				that.refreshMap();
+			} )
+		},
+
+		refreshMap: function()
+		{
+			var zoom = this.map.getZoom(),
+				center = this.map.getCenter();
+
+			if ( this.map )
+			{
+				google.maps.event.trigger( this.map, 'resize' );
+				this.map.setZoom( zoom );
+				this.map.setCenter( center );
+			}
 		},
 
 		// Autocomplete address
